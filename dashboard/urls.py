b836d635bf75bd38a9a71fabb081a6e65df61919
alefsanderribeiro <alefsander.pvh14@gmail.com>
2024-07-27 12:04:32 -0400

    Site Automatizando Tarefas

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard_home, name="dashboard_home"),
     path('home2', views.dashboard_hom2, name="dashboard_home2"),
+    path('table', views.exibir_tabela, name="minha_tabela"),
 ]
