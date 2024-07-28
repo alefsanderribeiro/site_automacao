648149db1e8cac0dcbfb3c2b83fb796a7caf707e
alefsanderribeiro <alefsander.pvh14@gmail.com>
2024-07-27 21:32:11 -0400

    Ajustes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_home, name="dashboard_home"),
+    path('', views.dashboard, name="dashboard"),
+    path('page1', views.dashboard_hom2, name="dash_pg1"),
+    path('page2', views.dashboard_hom2, name="dash_pg2"),
+    path('page3', views.dashboard_hom2, name="dash_pg3"),
     path('home2', views.dashboard_hom2, name="dashboard_home2"),
     path('table', views.exibir_tabela, name="minha_tabela"),
 ]
